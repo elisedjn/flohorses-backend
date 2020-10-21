174f06f5c1bf8ef874e46d7c7c27c771adb26d98
Elise Djn <elisedjn@protonmail.com>
2020-10-21 19:28:55 +0300

    ready for first deploy

---
+++ public/precache-manifest.11fb60d7dc6065e8bab3432f3371967d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e446c2bd5d0c0fe6db1babf3d532d64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad7ffc406dbdc4249e6a",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "2a4a7d934ebe2ef98858",
+    "url": "/static/css/main.ecac202d.chunk.css"
+  },
+  {
+    "revision": "ad7ffc406dbdc4249e6a",
+    "url": "/static/js/2.2bbf7bb3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2bbf7bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4a7d934ebe2ef98858",
+    "url": "/static/js/main.2a98a50f.chunk.js"
+  },
+  {
+    "revision": "b6f9c1316341529edc45",
+    "url": "/static/js/runtime-main.ac6a5ae7.js"
+  }
+]);
